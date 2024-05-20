673060e975066f271f44c3c5df4cefa1ca00185e
AQLI21 <aqli122421@gmail.com>
2024-05-20 18:17:19 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main" @dragenter.prevent @dragover.prevent @drop.prevent="onDrop">
-    <progress v-if="uploadProgress !== null" :value="uploadProgress" max="100"></progress>
-    <UploadPopup v-model="showUploadPopup" @upload="onUploadClicked" @createFolder="createFolder"></UploadPopup>
+    <progress
+      v-if="uploadProgress !== null"
+      :value="uploadProgress"
+      max="100"
+    ></progress>
+    <UploadPopup
+      v-model="showUploadPopup"
+      @upload="onUploadClicked"
+      @createFolder="createFolder"
+    ></UploadPopup>
     <button class="upload-button circle" @click="showUploadPopup = true">
       <img
         style="filter: invert(100%)"
@@ -24,35 +32,121 @@
             title="Menu"
             style="display: block; margin: 4px"
           >
+            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
             <path
-              d="M120 256c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm160 0c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm104 56c-30.9 0-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56z"
+              d="M120 256c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm160 0c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm104 56c-30.9 0-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56s-25.1 56-56 56z"
             />
           </svg>
         </button>
         <Menu
           v-model="showMenu"
-          :items="[
-            { text: '名称A-Z' },
-            { text: '大小↑' },
-            { text: '大小↓' },
-            { text: '粘贴' },
-            { text: '关于 LI Innovation 共享存储库' }  <!-- 添加新项 -->
-          ]"
+          :items="[{ text: '名称A-Z' }, { text: '大小↑' } ,{ text: '大小↓' }, { text: '粘贴' }]"
           @click="onMenuClick"
         />
       </div>
     </div>
     <ul class="file-list">
-      <!-- 省略其余代码 -->
+      <li v-if="cwd !== ''">
+        <div
+          tabindex="0"
+          class="file-item"
+          @click="cwd = cwd.replace(/[^\/]+\/$/, '')"
+          @contextmenu.prevent
+        >
+          <div class="file-icon">
+            <img
+              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+              width="36"
+              height="36"
+              alt="Folder"
+            />
+          </div>
+          <span class="file-name">..</span>
+        </div>
+      </li>
+      <li v-for="folder in filteredFolders" :key="folder">
+        <div
+          tabindex="0"
+          class="file-item"
+          @click="cwd = folder"
+          @contextmenu.prevent="
+            showContextMenu = true;
+            focusedItem = folder;
+          "
+        >
+          <div class="file-icon">
+            <img
+              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+              width="36"
+              height="36"
+              alt="Folder"
+            />
+          </div>
+          <span
+            class="file-name"
+            v-text="folder.match(/.*?([^/]*)\/?$/)[1]"
+          ></span>
+          <div style="margin-right: 10px;margin-left: auto;"
+            @click.stop="
+              showContextMenu = true;
+              focusedItem = folder;
+            "
+            >
+              <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;"><path fill="currentColor" d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z"></path></svg>
+          </div>
+        </div>
+      </li>
+      <li v-for="file in filteredFiles" :key="file.key">
+        <div
+          @click="preview(`/raw/${file.key}`)"
+          @contextmenu.prevent="
+            showContextMenu = true;
+            focusedItem = file;
+          "
+        >
+          <div class="file-item">
+            <MimeIcon
+              :content-type="file.httpMetadata.contentType"
+              :thumbnail="
+                file.customMetadata.thumbnail
+                  ? `/raw/_$flaredrive$/thumbnails/${file.customMetadata.thumbnail}.png`
+                  : null
+              "
+            />
+            <div>
+              <div class="file-name" v-text="file.key.split('/').pop()"></div>
+              <div class="file-attr">
+                <span v-text="new Date(file.uploaded).toLocaleString()"></span>
+                <span v-text="formatSize(file.size)"></span>
+              </div>
+            </div>
+            <div style="margin-right: 10px;margin-left: auto;"
+            @click.stop="
+              showContextMenu = true;
+              focusedItem = file;
+            "
+            >
+              <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;"><path fill="currentColor" d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z"></path></svg>
+            </div>
+          </div>
+        </div>
+      </li>
     </ul>
     <div v-if="loading" style="margin-top: 12px; text-align: center">
       <span>加载中...</span>
     </div>
-    <div v-else-if="!filteredFiles.length && !filteredFolders.length" style="margin-top: 12px; text-align: center">
+    <div
+      v-else-if="!filteredFiles.length && !filteredFolders.length"
+      style="margin-top: 12px; text-align: center"
+    >
       <span>没有文件</span>
     </div>
     <Dialog v-model="showContextMenu">
-      <div v-text="focusedItem.key || focusedItem" class="contextmenu-filename" @click.stop.prevent></div>
+      <div
+        v-text="focusedItem.key || focusedItem"
+        class="contextmenu-filename"
+        @click.stop.prevent
+      ></div>
       <ul v-if="typeof focusedItem === 'string'" class="contextmenu-list">
         <li>
           <button @click="copyLink(`/?p=${encodeURIComponent(focusedItem)}`)">
@@ -60,7 +154,10 @@
           </button>
         </li>
         <li>
-          <button style="color: red" @click="removeFile(focusedItem + '_$folder$')">
+          <button
+            style="color: red"
+            @click="removeFile(focusedItem + '_$folder$')"
+          >
             <span>删除</span>
           </button>
         </li>
@@ -97,8 +194,6 @@
 </template>
 
 <script>
-import axios from 'axios'; // 添加此导入
-
 import {
   generateThumbnail,
   blobDigest,
@@ -111,30 +206,28 @@ import MimeIcon from "./MimeIcon.vue";
 import UploadPopup from "./UploadPopup.vue";
 
 export default {
-  data() {
-    return {
-      cwd: new URL(window.location).searchParams.get("p") || "",
-      files: [],
-      folders: [],
-      clipboard: null,
-      focusedItem: null,
-      loading: false,
-      order: null,
-      search: "",
-      showContextMenu: false,
-      showMenu: false,
-      showUploadPopup: false,
-      uploadProgress: null,
-      uploadQueue: [],
-    };
-  },
+  data: () => ({
+    cwd: new URL(window.location).searchParams.get("p") || "",
+    files: [],
+    folders: [],
+    clipboard: null,
+    focusedItem: null,
+    loading: false,
+    order: null,
+    search: "",
+    showContextMenu: false,
+    showMenu: false,
+    showUploadPopup: false,
+    uploadProgress: null,
+    uploadQueue: [],
+  }),
 
   computed: {
     filteredFiles() {
       let files = this.files;
       if (this.search) {
         files = files.filter((file) =>
-          file.key.split("/").slice(-1)[0].includes(this.search)
+          file.key.split("/").pop().includes(this.search)
         );
       }
       return files;
@@ -185,13 +278,15 @@ export default {
       this.folders = [];
       this.loading = true;
       fetch(`/api/children/${this.cwd}`)
-        .then((res) => res.json
+        .then((res) => res.json())
         .then((files) => {
           this.files = files.value;
-          if (this.order === "size") {
-            this.files.sort((a, b) => b.size - a.size);
-          } else {
-            this.files.sort((a, b) => a.key.localeCompare(b.key));
+          if (this.order) {
+            this.files.sort((a, b) => {
+              if (this.order === "size") {
+                return b.size - a.size;
+              }
+            });
           }
           this.folders = files.folders;
           this.loading = false;
@@ -214,9 +309,7 @@ export default {
         files = [...ev.dataTransfer.items]
           .filter((item) => item.kind === "file")
           .map((item) => item.getAsFile());
-      } else {
-        files = ev.dataTransfer.files;
-      }
+      } else files = ev.dataTransfer.files;
       this.uploadFiles(files);
     },
 
@@ -226,78 +319,157 @@ export default {
           this.order = null;
           break;
         case "大小↑":
-          this.order = "size";
+          this.order = "大小↑";
           break;
         case "大小↓":
-          this.order = "size";
+          this.order = "大小↓";
           break;
         case "粘贴":
           return this.pasteFile();
-        case "关于 LI Innovation 共享存储库":
-          window.open("https://example.com/about", "_blank");
-          break;
       }
-      this.fetchFiles();
+      this.files.sort((a, b) => {
+        if (this.order === "大小↑") {
+          return a.size - b.size;
+        } else if (this.order === "大小↓") {
+          return b.size - a.size;
+        } else {
+          return a.key.localeCompare(b.key);
+        }
+      });
     },
 
     onUploadClicked(fileElement) {
       if (!fileElement.value) return;
       this.uploadFiles(fileElement.files);
+      this.showUploadPopup = false;
+      fileElement.value = null;
     },
 
-    pasteFile() {
-      const file = this.clipboard;
-      if (typeof file === "string") {
-        return this.copyPaste(file, this.cwd + file.split("/").pop());
+    preview(filePath){
+      window.open(filePath);
+    },
+
+    async pasteFile() {
+      if (!this.clipboard) return;
+      let newName = window.prompt("Rename to:");
+      if (newName === null) return;
+      if (newName === "") newName = this.clipboard.split("/").pop();
+      await this.copyPaste(this.clipboard, `${this.cwd}${newName}`);
+      this.fetchFiles();
+    },
+
+    async processUploadQueue() {
+      if (!this.uploadQueue.length) {
+        this.fetchFiles();
+        this.uploadProgress = null;
+        return;
       }
-      return this.uploadFiles([file]);
-    },
 
-    async removeFile(file) {
+      /** @type File **/
+      const { basedir, file } = this.uploadQueue.pop(0);
+      let thumbnailDigest = null;
+
+      if (file.type.startsWith("image/") || file.type === "video/mp4") {
+        try {
+          const thumbnailBlob = await generateThumbnail(file);
+          const digestHex = await blobDigest(thumbnailBlob);
+
+          const thumbnailUploadUrl = `/api/write/items/_$flaredrive$/thumbnails/${digestHex}.png`;
+          try {
+            await axios.put(thumbnailUploadUrl, thumbnailBlob);
+            thumbnailDigest = digestHex;
+          } catch (error) {
+            fetch("/api/write/")
+              .then((value) => {
+                if (value.redirected) window.location.href = value.url;
+              })
+              .catch(() => {});
+            console.log(`Upload ${digestHex}.png failed`);
+          }
+        } catch (error) {
+          console.log(`Generate thumbnail failed`);
+        }
+      }
+
       try {
-        if (window.confirm(`Are you sure you want to delete ${file}?`)) {
-          await axios.delete(`/api/write/items/${file}`);
-          this.fetchFiles();
+        const uploadUrl = `/api/write/items/${basedir}${file.name}`;
+        const headers = {};
+        const onUploadProgress = (progressEvent) => {
+          var percentCompleted =
+            (progressEvent.loaded * 100) / progressEvent.total;
+          this.uploadProgress = percentCompleted;
+        };
+        if (thumbnailDigest) headers["fd-thumbnail"] = thumbnailDigest;
+        if (file.size >= SIZE_LIMIT) {
+          await multipartUpload(`${basedir}${file.name}`, file, {
+            headers,
+            onUploadProgress,
+          });
+        } else {
+          await axios.put(uploadUrl, file, { headers, onUploadProgress });
         }
       } catch (error) {
-        console.log(`Delete ${file} failed`, error);
+        fetch("/api/write/")
+          .then((value) => {
+            if (value.redirected) window.location.href = value.url;
+          })
+          .catch(() => {});
+        console.log(`Upload ${file.name} failed`, error);
       }
+      setTimeout(this.processUploadQueue);
     },
 
-    renameFile(file) {
-      const dest = window.prompt("重命名文件为", file);
-      if (!dest) return;
-      const prefix = file.split("/").slice(0, -1).join("/") + "/";
-      return this.copyPaste(file, prefix + dest).then(() =>
-        this.removeFile(file)
-      );
+    async removeFile(key) {
+      if (!window.confirm(`确定要删除 ${key} 吗？`)) return;
+      await axios.delete(`/api/write/items/${key}`);
+      this.fetchFiles();
     },
 
-    async uploadFiles(files) {
-      if (!files.length) return;
-      this.uploadQueue = [...this.uploadQueue, ...files];
-      const next = async () => {
-        const file = this.uploadQueue.shift();
-        if (!file) return this.fetchFiles();
-        try {
-          this.uploadProgress = 0;
-          await multipartUpload(
-            file,
-            `${this.cwd}${file.webkitRelativePath || file.name}`,
-            (progress) => (this.uploadProgress = progress)
-          );
-          this.uploadProgress = null;
-        } catch (error) {
-          console.log(`Upload ${file.name} failed`, error);
-        }
-        await next();
-      };
-      next();
+    async renameFile(key) {
+      const newName = window.prompt("重命名为:");
+      if (!newName) return;
+      await this.copyPaste(key, `${this.cwd}${newName}`);
+      await axios.delete(`/api/write/items/${key}`);
+      this.fetchFiles();
+    },
+
+    uploadFiles(files) {
+      if (this.cwd && !this.cwd.endsWith("/")) this.cwd += "/";
+
+      const uploadTasks = Array.from(files).map((file) => ({
+        basedir: this.cwd,
+        file,
+      }));
+      this.uploadQueue.push(...uploadTasks);
+      setTimeout(() => this.processUploadQueue());
     },
   },
 
-  mounted() {
-    this.fetchFiles();
+  watch: {
+    cwd: {
+      handler() {
+        this.fetchFiles();
+        const url = new URL(window.location);
+        if ((url.searchParams.get("p") || "") !== this.cwd) {
+          this.cwd
+            ? url.searchParams.set("p", this.cwd)
+            : url.searchParams.delete("p");
+          window.history.pushState(null, "", url.toString());
+        }
+        document.title = `${
+          this.cwd.replace(/.*\/(?!$)|\//g, "") || "/"
+        } - 文件库`;
+      },
+      immediate: true,
+    },
+  },
+
+  created() {
+    window.addEventListener("popstate", (ev) => {
+      const searchParams = new URL(window.location).searchParams;
+      if (searchParams.get("p") !== this.cwd)
+        this.cwd = searchParams.get("p") || "";
+    });
   },
 
   components: {
@@ -308,3 +480,37 @@ export default {
   },
 };
 </script>
+
+<style>
+.main {
+  height: 100%;
+}
+
+.app-bar {
+  position: sticky;
+  top: 0;
+  padding: 8px;
+  background-color: white;
+  display: flex;
+}
+
+.menu-button {
+  display: flex;
+  position: relative;
+  margin-left: 4px;
+}
+
+.menu-button > button {
+  transition: background-color 0.2s ease;
+}
+
+.menu-button > button:hover {
+  background-color: whitesmoke;
+}
+
+.menu {
+  position: absolute;
+  top: 100%;
+  right: 0;
+}
+</style>
